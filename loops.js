1ed240b8e6e337806aaf41c586409feaace70e28
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-02-07 04:33:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,17 +22,13 @@
   
   
   
-   function doWhileLoop(array) {
-   function maybeTrue() {
-     return Math.random() >= 0.5;
+  function doWhileLoop(array){
+ function maybeTrue(){
+    return Math.random() >= 0.5;
   }
-  
-  
-   do {
-    array.pop;
-   } while (array.length > 0 && maybeTrue());
-   
-   
-     return array;
-   }
-  
+
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+  return array;
+  }
